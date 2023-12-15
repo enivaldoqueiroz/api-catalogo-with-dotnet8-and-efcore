8cf18e7f887be9f7fe75c59d6e5581010a1ead6f
José Queiroz <jose.queiroz@setling.pt>
2023-12-15 15:25:23 +0000

    Implementação do projeto ApiCatalogo e Entidades. Instalação dos pacote de contexto e ferramentas EF Core

---
+++ api-catalogo/api-catalogo/Models/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace api_catalogo.Models;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+
+    public string? Nome { get; set; }
+    public string? ImagemUrl { get; set; }
+}
