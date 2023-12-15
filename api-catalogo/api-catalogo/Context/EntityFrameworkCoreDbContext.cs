d88b384b181ac0ebf7b5aa0885b221b4757e7e07
José Queiroz <jose.queiroz@setling.pt>
2023-12-15 15:36:15 +0000

    Implementação da classe de contexto para acessar o banco de dados

---
+++ api-catalogo/api-catalogo/Context/EntityFrameworkCoreDbContext.cs
@@ -0,0 +1,12 @@
+﻿using api_catalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_catalogo.Context;
+
+public class EntityFrameworkCoreDbContext : DbContext
+{
+    protected EntityFrameworkCoreDbContext(DbContextOptions<EntityFrameworkCoreDbContext> options) : base(options) {  }
+
+    public DbSet<Categoria>? Categorias { get; set; }
+    public DbSet<Produto>? Produtos { get; set; }
+}
