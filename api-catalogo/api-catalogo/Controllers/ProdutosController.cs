4c79eca3d2ca52a4a602b3448364ef7d6cb0e054
José Queiroz <jose.queiroz@setling.pt>
2023-12-21 11:55:08 +0000

    Implementação do Endpoint Delete

--- api-catalogo/api-catalogo/Controllers/ProdutosController.cs
+++ api-catalogo/api-catalogo/Controllers/ProdutosController.cs
@@ -111,5 +111,20 @@ namespace api_catalogo.Controllers
 
             return Ok(produto);
         }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            //var produto = _context.Produtos.Find(id);
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+            if (produto is null)
+                return NotFound();
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
     }
 }
