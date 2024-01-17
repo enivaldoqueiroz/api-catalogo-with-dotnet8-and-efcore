27f81e83f0ab4cc1c4a01bd20aaa1902430d45e7
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2024-01-17 18:08:52 +0000

    Criação da projeto MVC

---
+++ web-application-mvc-with-identity-dotnet8/web-application-mvc-identity-dotnet8/web-application-mvc-identity-dotnet8/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace web_application_mvc_identity_dotnet8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
